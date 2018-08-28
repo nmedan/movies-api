8598ce0ab8cb91ec264edf8c4156c14cbacf5020
nmedan <nmedan92@gmail.com>
2018-08-28 19:23:25 +0200

    Zadatak 2

--- app/Movie.php
+++ app/Movie.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    //
+    protected $fillable = ['title', 'director', 'imageUrl', 'duration',
+    'releaseDate', 'genre'];
 }
